c71089de718f630a65915de08f70abe4f92af7c9
geekbrains <mamamia89678524930@gmail.com>
2023-04-09 13:28:20 +0500

    Добавил Домашнее задание №4

---
+++ Programm.java
@@ -0,0 +1,12 @@
+public class Programm {
+    public static void main(String[] args) {
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+        tree.add(5);
+        tree.add(10);
+        tree.add(3);
+        tree.add(7);
+        tree.add(12);
+        tree.add(1);
+
+    }
+}
